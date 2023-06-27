1816966d5f89a57f3ff2bb16d72c2e23123d677a
Pravin Bhadane <pravinbhadane60@gmail.com>
2023-06-27 22:01:47 +0530

    Add project files.

---
+++ stringBuilder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder("Hello to all");
+  
+
+        StringBuilder sb2 = new StringBuilder(20);
+
+
+        Console.WriteLine(sb);
+        Console.WriteLine(sb.GetHashCode());
+
+        sb.Append(" Good Morning");
+        Console.WriteLine(sb);
+        Console.WriteLine(sb.GetHashCode());
+
+        sb.Insert(5, " TEST DATA");
+        sb.Remove(5, 5);
+
+        sb.Replace("DATA", "new data");
+        Console.WriteLine(sb);
+
+
+        StringBuilder sb3 = new StringBuilder("Your amount is ");     
+
+        sb3.AppendFormat("{0:C}", 25000); 
+
+        Console.WriteLine(sb3);
+
+    }
+}
